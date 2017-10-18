5ffbe1c7700ff0f0b6c6f4f5338533d225c0c513
墨宇 <zzh_hz@126.com>
2017-10-18 15:43:32 +0800

    view的工作原理

---
+++ ViewPrinciple/src/main/java/com/zzh/view/principle/MainActivity.java
@@ -0,0 +1,21 @@
+package com.zzh.view.principle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by ZZH on 2017/10/18
+ *
+ * @date: 2017/10/18 下午3:41
+ * @email: zzh_hz@126.com
+ * @QQ: 1299234582
+ * @author: zzh
+ * @Description: view 的工作原理
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
