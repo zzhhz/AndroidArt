f1ab1e57d68a28ea38a06ce05a806c93e45ba8cf
墨宇 <zzh_hz@126.com>
2017-10-30 15:56:22 +0800

    动画，WindowManager

---
+++ UnderstandWindowManger/src/main/java/com/zzh/window/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zzh.window;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String sString = "sString";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
