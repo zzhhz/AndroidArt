6d9a811702221515651990fcd564fc547c9119fc
墨宇 <zzh_hz@126.com>
2017-11-03 12:23:30 +0800

    动画，WindowManager2

--- UnderstandWindowManger/src/main/java/com/zzh/window/MainActivity.java
+++ UnderstandWindowManger/src/main/java/com/zzh/window/MainActivity.java
@@ -1,14 +1,36 @@
 package com.zzh.window;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+/**
+ * Created by ZZH on 2017/10/31
+ *
+ * @date: 2017/10/31 下午1:50
+ * @email: zzh_hz@126.com
+ * @QQ: 1299234582
+ * @author: zzh
+ * @Description:
+ */
 public class MainActivity extends AppCompatActivity {
+    /**
+     * TAG
+     */
     public static final String sString = "sString";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, sString, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent();
+        ComponentName service = startService(intent);
+
+
+
     }
 }
