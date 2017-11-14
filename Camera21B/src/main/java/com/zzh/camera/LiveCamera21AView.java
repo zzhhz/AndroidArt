cb6eeaed529f437a3cf51fc2901d1d47d2dbb704
墨宇 <zzh_hz@126.com>
2017-11-14 09:34:04 +0800

    surface view 调试相机 camera2。简单封装

--- Camera21B/src/main/java/com/zzh/camera/LiveCamera21AView.java
+++ Camera21B/src/main/java/com/zzh/camera/LiveCamera21AView.java
@@ -1,5 +1,10 @@
 package com.zzh.camera;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
 /**
  * Created by user on 2017/11/10.
  *
@@ -9,10 +14,33 @@ package com.zzh.camera;
  * @author: zzh
  * @description:
  */
-public class LiveCamera21AView {
+public class LiveCamera21AView extends SurfaceView implements SurfaceHolder.Callback{
 
 
-    public void openCamera21a(){
+    public LiveCamera21AView(Context context) {
+        super(context);
+    }
+
+    public LiveCamera21AView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void openCamera21a() {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
 
     }
 }
